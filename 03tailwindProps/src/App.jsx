04b16905d2806a0521f35e33bff2ba4c9bdc933d
itsYashKhare <kharey02@gmail.com>
2024-01-02 19:18:36 +0530

    learing about components  with tailwind  properties & props functionality

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  let obj ={
+    username:"Yash",
+    age:21
+  }
+  
+  let newArr =[1,2,3,4];
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl m-5 mb-4'>Tailwind</h1>
+      <Card username ="Code with me"  btnText ="click me"  />
+      <Card username="Yash" />
+    </>
+  )
+}
+
+export default App
